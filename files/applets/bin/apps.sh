983e4031129a8c161c43c10679007446d14dc746
megvadulthangya <67601373+megvadulthangya@users.noreply.github.com>
2025-09-28 20:06:16 +0200

    Update apps.sh

--- files/applets/bin/apps.sh
+++ files/applets/bin/apps.sh
@@ -22,11 +22,11 @@ elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
 fi
 
 # CMDs (add your apps here)
-term_cmd='alacritty'
+term_cmd='tilix'
 file_cmd='thunar'
 text_cmd='geany'
-web_cmd='firefox'
-music_cmd='alacritty -e ncmpcpp'
+web_cmd='brave'
+music_cmd='tilix -e ncmpcpp'
 setting_cmd='xfce4-settings-manager'
 
 # Options
